ad356f9b522c51d4fe5cecc3c86763189a1c68d3
Ahmet Erdoğan <ahmeterdgn6@gmail.com>
2019-07-10 01:16:09 +0300

    Organizing

--- www/js/api.js
+++ www/js/api.js
@@ -9,16 +9,16 @@ $(document).ready(function(){
       for (var i = data.data.Events.length - 1; i >= 0; i--) {
           items.push("<div class='col s12 '><div class='card horizontal'><div class='card-stacked'><div class='card-content'>"+
           "<p><h4>"+data.data.Events[i].year+"</h4>"+ data.data.Events[i].text +"</p></div><div class='card-action'>"+
-         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Events[i].links[0].link+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
+         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Events[i].year , data.data.Events[i].text+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
          "</div></div></div></div>"
          );
-   
       }
           $("<div>", {
         "class": "row",
         html: items.join("")
     }).appendTo("div.tarih");
        $(".overlay").hide();
+
       });
 
 });
@@ -38,7 +38,7 @@ $('#Events').click(function(){
       for (var i = data.data.Events.length - 1; i >= 0; i--) {
                items.push("<div class='col s12 '><div class='card horizontal'><div class='card-stacked'><div class='card-content'>"+
           "<p><h4>"+data.data.Events[i].year+"</h4>"+ data.data.Events[i].text +"</p></div><div class='card-action'>"+
-         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Events[i].links[0].link+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
+         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Events[i].year , data.data.Events[i].text+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
          "</div></div></div></div>"
          );
 
@@ -66,7 +66,7 @@ $(".overlay").show();
       for (var i = data.data.Births.length - 1; i >= 0; i--) {
                items.push("<div class='col s12 '><div class='card horizontal'><div class='card-stacked'><div class='card-content'>"+
           "<p><h4>"+data.data.Births[i].year+"</h4>"+ data.data.Births[i].text +"</p></div><div class='card-action'>"+
-         "<a href='#' class='right ' onclick=\"window.plugins.socialsharing.share('"+data.data.Births[i].links[0].link+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
+         "<a href='#' class='right ' onclick=\"window.plugins.socialsharing.share('"+data.data.Births[i].year , data.data.Births[i].text+"');\"><i class='far fa-share-square fa-2x'></i></a>"+
          "</div></div></div></div>"
          );
      
@@ -92,7 +92,7 @@ $(".overlay").show();
       for (var i = data.data.Deaths.length - 1; i >= 0; i--) {
           items.push("<div class='col s12 '><div class='card horizontal'><div class='card-stacked'><div class='card-content'>"+
           "<p><h4>"+data.data.Deaths[i].year+"</h4>"+ data.data.Deaths[i].text +"</p></div><div class='card-action'>"+
-         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Deaths[i].links[0].link+"');\" ><i class='far fa-share-square fa-2x'></i></a>"+
+         "<a href='#' class='right' onclick=\"window.plugins.socialsharing.share('"+data.data.Deaths[i].year , data.data.Deaths[i].text+"');\" ><i class='far fa-share-square fa-2x'></i></a>"+
          "</div></div></div></div>"
          );
 
